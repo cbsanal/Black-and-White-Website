741cafedab83abc692a75e490c55dfde7dd1f258
unknown <sanalcanberk@gmail.com>
2020-10-26 12:29:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Pencil-Drawing-Website
+
+*Proje HTML/CSS ve JavaScript kullanılarak yapılmıştır.
+
+*Tasarımı tamamen responsive yapabilmek amacıyla buna uygun CSS özellikleri kullanılmıştır.
+
+<img src="./images/github-images/anasayfa.jpg">
+<img src="./images/github-images/anasayfa-mobil.jpg" width="200">
+
+*Sitede bulunan animasyonlar için AOS(Animate On Scroll) kütüphanesi kullanılmıştır.
+
+*Sitenin yüklenme hızını arttırmak amacıyla bütün resimler olabildiğince küçültülmüştür ve sitede yer alan youtube videoları kullanıcı tıklayana kadar yüklenmemektedir.
+
+*Proje' yi indirmek için sağ üstte bulunan Code yazısına tıkladıktan sonra, Download ZIP diyebilirsiniz, indirdikten sonra index.html dosyasını açarak siteyi görebilirsiniz veya
+direk bu siteyi ziyaret edebilirsiniz: https://blackandwhitewebsite.netlify.app
+
---
+++ js/app.js
@@ -0,0 +1,43 @@
+const menu = document.querySelector('.menu');
+const slideNav = document.querySelector('ul');
+const lines = document.querySelectorAll('.line');
+const body = document.querySelector('body');
+const html = document.querySelector('html');
+const links = slideNav.querySelectorAll('a');
+
+let menuOff = true;
+
+function openNav() {
+    html.classList.add('body-fixed');
+    body.classList.add('body-fixed');
+    slideNav.style.transform = `translateX(-100%)`;
+    lines[0].style.transform = 'rotateZ(45deg) translateY(420%)';
+    lines[1].style.opacity = '0';
+    lines[2].style.transform = 'rotateZ(-45deg) translateY(-420%)';
+}
+
+function closeNav() {
+    html.classList.remove('body-fixed');
+    body.classList.remove('body-fixed');
+    slideNav.style.transform = 'translateX(0%)';
+    lines[0].style.transform = 'rotateZ(0) translateY(0)';
+    lines[1].style.opacity = '1';
+    lines[2].style.transform = 'rotateZ(0) translateY(0)';
+}
+
+menu.addEventListener('click', function () {
+    if (menuOff === true) {
+        openNav();
+        menuOff = false;
+    } else {
+        closeNav();
+        menuOff = true;
+    }
+})
+
+links.forEach(link => {
+    link.addEventListener('click', function () {
+        closeNav();
+        menuOff = true;
+    })
+})
